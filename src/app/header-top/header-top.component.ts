e6096ed886f4cb8ee3d65db00a9d4a08c58be38d
y-liang <yan@yliang.net>
2023-04-11 08:46:54 -0700

    update styles

--- src/app/header-top/header-top.component.ts
+++ src/app/header-top/header-top.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderTopComponent implements OnInit {
 
 
+  squares: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
 
 
   constructor() { }
